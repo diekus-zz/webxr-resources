9dcfeb943a419222f755af4aaed612d96c23789e
diekus <diekus@gmail.com>
2018-08-01 20:11:32 +0100

    update

--- aframe/components/a-layout.js
+++ aframe/components/a-layout.js
@@ -2,12 +2,16 @@
 
 AFRAME.registerComponent('a-layout', {
     schema: {
-        shape:{type: 'string', default: 'circle'}, //grid, circle, line
+        shape:{type: 'string', default: 'circle'}, //grid, circle, spiral
         modifier:{type:'int', default:4}, // modifier will be radius if circle, and columns if grid
         items:{type:'array', default:[]},
-        spacer:{type:'int', default:3},
+        spacer_h:{type:'int', default:3},
+        spacer_v:{type:'int', default:1},
+        spiral_spins:{type:'float', default:3.0},
+        spiral_delta_v:{type:'float', default:.5},
         animation:{type:'bool', default:false},
-        dur_anim:{type: 'int', default:500}
+        dur_anim:{type: 'int', default:500},
+
     },
     init: function(){
         this.data.items = this.el.children;
@@ -34,6 +38,9 @@ AFRAME.registerComponent('a-layout', {
             case 'grid':
                 this.setGridLayout();
                 break;
+            case 'spiral':
+              this.setSpiralLayout();
+              break;
         }
     },
     setCircleLayout : function(){
@@ -44,7 +51,7 @@ AFRAME.registerComponent('a-layout', {
                 let tween_rot = new AFRAME.TWEEN.Tween(this.data.items[i].object3D.rotation).to({y:toRadians(-(90-delta * -i))}, this.data.dur_anim*2);
                 tween_rot.easing(TWEEN.Easing.Cubic.Out);
                 tween_rot.start();
-                let tween_pos = new AFRAME.TWEEN.Tween(this.data.items[i].object3D.position).to({x:(this.data.modifier * Math.cos(toRadians(delta * i))), y: 1, z:(this.data.modifier * Math.sin(toRadians(delta * i)))}, this.data.dur_anim);
+                let tween_pos = new AFRAME.TWEEN.Tween(this.data.items[i].object3D.position).to({x:(this.data.modifier * Math.cos(toRadians(delta * i))), y: 0, z:(this.data.modifier * Math.sin(toRadians(delta * i)))}, this.data.dur_anim);
                 tween_pos.easing(TWEEN.Easing.Cubic.Out);
                 tween_pos.start();
             }
@@ -57,20 +64,47 @@ AFRAME.registerComponent('a-layout', {
     },
     setGridLayout : function(){
         let cols = this.data.modifier;
-        let item_w = this.data.spacer; 
-        let item_h = this.data.spacer;
+        let item_w = this.data.spacer_h; 
+        let item_h = this.data.spacer_v;
         let f =0, c = 0;
         for(i = 0; i < this.data.items.length; i++){
-            this.data.items[i].object3D.position.set(c * item_w, f * item_h, 0);
-            let tween_pos = new AFRAME.TWEEN.Tween(this.data.items[i].object3D.position).to({x:(c * item_w + this.data.spacer), y: (f * item_h), z:-6}, this.data.dur_anim);
+          this.data.items[i].object3D.rotation.set(0, 0, 0);
+          if(this.data.animation){
+            let tween_pos = new AFRAME.TWEEN.Tween(this.data.items[i].object3D.position).to({x:(c * item_w + this.data.spacer), y: (f * item_h), z:0}, this.data.dur_anim);
             tween_pos.easing(TWEEN.Easing.Cubic.Out);
             tween_pos.start();
-            this.data.items[i].object3D.rotation.set(0, 0, 0);
+          }
+          else{
+            this.data.items[i].object3D.position.set(c * item_w, f * item_h, 0);
+          }
+          //move on columns and rows
             c++;
             if (c == cols){
                 c=0;
                 f++;
             }
+        }
+    },
+    setSpiralLayout : function(){
+      let radius = this.data.modifier;
+      let current_v = 0.0;
+        let delta = (this.data.spiral_spins*360.0)/ this.data.items.length;
+        for(i = 0; i < this.data.items.length; i++){
+            if(this.data.animation){
+                let tween_rot = new AFRAME.TWEEN.Tween(this.data.items[i].object3D.rotation).to({y:toRadians(-(90-delta * -i))}, this.data.dur_anim*2);
+                tween_rot.easing(TWEEN.Easing.Cubic.Out);
+                tween_rot.start();
+      let current_v = 0.0;
+                let tween_pos = new AFRAME.TWEEN.Tween(this.data.items[i].object3D.position).to({x:(this.data.modifier * Math.cos(toRadians(delta * i))), y: current_v, z:(this.data.modifier * Math.sin(toRadians(delta * i)))}, this.data.dur_anim);
+                tween_pos.easing(TWEEN.Easing.Cubic.Out);
+                tween_pos.start();
+            }
+            else{
+                this.data.items[i].object3D.rotateY(toRadians(180-(90-delta * -i)));
+                this.data.items[i].object3D.position.set(radius * Math.cos(toRadians(delta * i)), current_v, radius * Math.sin(toRadians(delta * i)));
+            }
+            current_v = current_v + this.data.spiral_delta_v;
+            //this.data.items[i].addEventListener('click', layout_item_click);
         }
     },
     //for the time being refresh is set as a way to force layout after insertion of entity in runtime if needed
@@ -83,6 +117,9 @@ AFRAME.registerComponent('a-layout', {
             case 'grid':
                 this.setGridLayout();
                 break;
+            case 'spiral':
+              this.setSpiralLayout();
+              break;
         }
     }
 });
--- aframe/components/entity-generator.js
+++ aframe/components/entity-generator.js
@@ -1,15 +1,27 @@
 /* entity-generator aframe component by diekus */
+/* ATTENTION:
+The 'attributes' attribute of the entity-genertator uses a different type of delimiters since it is an CSS-like string notation inside CSS-like string notation. In order to be able to set attributes it uses '&' and '='. An 'attributes' subattribute looks like attributes:attribute1=value1&attribute2=value2
+*/
 
 AFRAME.registerComponent('entity-generator', {
     schema: {
         entity_name:{type: 'string', default: 'a-entity'}, 
         attributes:{type:'string', default:''}, 
         number_items:{type:'int', default:2},
+        id_generation:{type:'bool', default:false},
+        id_prefix:{type:'string', default:'gen_ent_'}
     },
     init: function(){
         for (i = 0; i < this.data.number_items; i++){
             let ne = document.createElement(this.data.entity_name);
-            ne.setAttribute('class', this.data.attributes);
+            atts = this.data.attributes.split('&');
+             for(a = 0; a < atts.length; a++){
+                 att = atts[a].split(':');
+                 att[1] != null?ne.setAttribute(att[0], att[1]):ne.setAttribute(att[0], '');
+              }
+              if(this.data.id_generation){
+                ne.setAttribute('id',  this.data.id_prefix + i);
+              }
             this.el.appendChild(ne);
             
         }
